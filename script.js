09bb04c927f5e3c4d5d53dc2fa308fcb917293a2
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-19 19:34:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // Your code here.
+const itemsContainer = document.getElementById('items');
+    let isDragging = false;
+    let startPosition = 0;
+    let currentTranslate = 0;
+
+    itemsContainer.addEventListener('mousedown', (e) => {
+      isDragging = true;
+      startPosition = e.clientX - itemsContainer.offsetLeft;
+      itemsContainer.classList.add('active');
+    });
+
+    itemsContainer.addEventListener('mouseup', () => {
+      isDragging = false;
+      itemsContainer.classList.remove('active');
+    });
+
+    itemsContainer.addEventListener('mousemove', (e) => {
+      if (!isDragging) return;
+      const currentPosition = e.clientX - itemsContainer.offsetLeft;
+      const distance = currentPosition - startPosition;
+      currentTranslate += distance;
+      itemsContainer.style.transform = `translateX(${currentTranslate}px)`;
+      startPosition = currentPosition;
+    });
+
+    itemsContainer.addEventListener('mouseleave', () => {
+      isDragging = false;
+      itemsContainer.classList.remove('active');
